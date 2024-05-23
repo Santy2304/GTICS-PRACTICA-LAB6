4ba3aa45fe11cf8e6593db86286423facb2e8379
Santy2304 <a20210751@pucp.edu.pe>
2024-05-23 09:35:35 -0500

    inicio

---
+++ src/main/java/com/example/practicalb6gtics/PracticaLb6GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicalb6gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaLb6GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaLb6GticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicalb6gtics/PracticaLb6GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicalb6gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaLb6GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
